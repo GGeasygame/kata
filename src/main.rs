80f6397066df54d78d59e7c1525a2099fbacb398
Jonas Kaufmann <jonas.kaufmann@sunrise.ch>
2024-06-05 11:49:20 +0200

    can execute program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{stdin, stdout, Write};
+use wordcount::count_words;
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::new();
+    print!("Enter text: ");
+    let _ = stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
+    println!("Number of words: {}", count_words(&s))
 }
