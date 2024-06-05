48224e1f18e278563023409a17c51fc11469fd7a
alwinmark <alwin.mark@css.ch>
2024-06-05 14:28:30 +0200

    Unittest for stopwords

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 use regex::Regex;
+use std::collections::HashSet;
 
-pub fn count_words(text: &str, vec1: Vec<&str>) -> i32 {
+pub fn count_words(text: &str, stop_list: Vec<&str>) -> i32 {
+    let filter_set: HashSet<_> = stop_list.into_iter().collect();
     let re = Regex::new(r"[a-zA-Z]+").unwrap();
-    match re.find(text) {
-        Some(value) => value.len() as i32,
-        None => 0
-    }
+
+    re.find_iter(text)
+        .filter(
+            |regex_match| !filter_set.contains(regex_match.as_str())
+        ).count() as i32
 }
 
 #[cfg(test)]
@@ -14,17 +17,17 @@ mod tests {
 
     #[test]
     fn test_count_words_little_lamb_poem() {
-        assert_eq!(5, count_words("marry had a little lamb", ))
+        assert_eq!(5, count_words("marry had a little lamb", vec![]))
     }
 
     #[test]
     fn test_count_words_text_with_two_whitespaces() {
-        assert_eq!(5, count_words("marry had a little  lamb", ))
+        assert_eq!(5, count_words("marry had a little  lamb", vec![]))
     }
 
     #[test]
     fn test_count_words_empty_string() {
-        assert_eq!(0, count_words("", ))
+        assert_eq!(0, count_words("", vec![]))
     }
 
     #[test]
