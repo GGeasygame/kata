5f45316a26fc18f1d7420ee1d956a0012c94525f
Jonas Kaufmann <jonas.kaufmann@sunrise.ch>
2024-06-05 11:53:20 +0200

    refactored main

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ use std::io::{stdin, stdout, Write};
 use wordcount::count_words;
 
 fn main() {
-    let mut s = String::new();
     print!("Enter text: ");
+    let s = read_user_input();
+    println!("Number of words: {}", count_words(&s))
+}
+
+fn read_user_input() -> String {
+    let mut s = String::new();
     let _ = stdout().flush();
     stdin().read_line(&mut s).expect("Did not enter a correct string");
     if let Some('\n') = s.chars().next_back() {
@@ -12,5 +17,5 @@ fn main() {
     if let Some('\r') = s.chars().next_back() {
         s.pop();
     }
-    println!("Number of words: {}", count_words(&s))
+    s
 }
