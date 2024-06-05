dd0fd1e7d9e43643669e87528f1546644794746e
Jonas Kaufmann <jonas.kaufmann@sunrise.ch>
2024-06-05 13:53:34 +0200

    stopwords.txt added + integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::io::Write;
 use std::process::{Child, Command, Stdio};
 
@@ -16,12 +17,38 @@ fn test_main_little_lamb_poem() {
         stdin.write_all(b"marry had a little lamb").expect("stdin not writable");
     }
 
-    assert_output(child);
+    assert_output("Enter text: Number of words: 5\n", child);
 }
 
-fn assert_output(child: Child) {
+#[test]
+fn test_main_little_lamb_poem_with_stopwords() {
+    let mut file = File::create("stopwords.txt").expect("cannot create file");
+    file.write_all(br#"
+    the
+    a
+    on
+    off
+    "#).expect("cannot write file");
+
+    let mut child = Command::new("cargo")
+        .arg("run")
+        .arg("--quiet")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("no output");
+
+    {
+        let stdin = child.stdin.as_mut().expect("no stdin received");
+        stdin.write_all(b"marry had a little lamb").expect("stdin not writable");
+    }
+
+    assert_output("Enter text: Number of words: 4\n", child);
+}
+
+fn assert_output(expected: &str, child: Child) {
     let enter_text_prompt = child.wait_with_output().expect("no stdout received");
     let out = String::from_utf8_lossy(&enter_text_prompt.stdout);
 
-    assert_eq!("Enter text: Number of words: 5\n", out);
+    assert_eq!(expected, out);
 }
