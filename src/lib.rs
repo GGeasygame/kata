1e8d47949b956428d56178627f5c5a5f9548e2a6
Jonas Kaufmann <jonas.kaufmann@sunrise.ch>
2024-06-05 11:32:20 +0200

    two whitespaces works

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
+use regex::Regex;
+
 pub fn count_words(text: &str) -> i32 {
-    text.split(" ").count() as i32
+    let re = Regex::new(r"[a-zA-Z]+").unwrap();
+    re.find(text).unwrap().len() as i32
+
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +15,8 @@ mod tests {
         assert_eq!(5, count_words("marry had a little lamb"))
     }
 
-
+    #[test]
+    fn test_count_words_text_with_two_whitespaces() {
+        assert_eq!(5, count_words("marry had a little  lamb"))
+    }
 }
