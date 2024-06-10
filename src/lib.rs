6845b9e1941ef01cc55109f3858a273f643d0c6e
Jonas Kaufmann <jonas.kaufmann@sunrise.ch>
2024-06-10 13:48:40 +0200

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,36 @@
-use regex::Regex;
 use std::collections::HashSet;
+use regex::{Match, Regex};
 
-pub fn count_words(text: &str, stop_list: Vec<String>) -> i32 {
-    let filter_set: HashSet<_> = stop_list.into_iter().collect();
-    let re = Regex::new(r"[a-zA-Z-]+").unwrap();
 
-    re.find_iter(text)
-        .filter(
-            |regex_match| !filter_set.contains(regex_match.as_str())
-        ).count() as i32
+const WORD_REGEX: &str = r"[a-zA-Z-]+";
+
+pub fn count_words(text: &String, stop_list: &HashSet<String>) -> i32 {
+    let regex = Regex::new(WORD_REGEX).unwrap();
+    find_matches(text, stop_list, &regex).count() as i32
 }
 
-pub fn count_unique_words(text: &str, stop_list: Vec<String>) -> i32 {
-    let filter_set: HashSet<_> = stop_list.into_iter().collect();
-    let re = Regex::new(r"[a-zA-Z-]+").unwrap();
-
-    HashSet::<&str>::from_iter(
-        re.find_iter(text)
-            .filter(|regex_match| !filter_set.contains(regex_match.as_str()))
+pub fn count_unique_words(text: &String, stop_list: &HashSet<String>) -> i32 {
+    let regex = Regex::new(WORD_REGEX).unwrap();
+   HashSet::<&str>::from_iter(
+       find_matches(text, stop_list, &regex)
             .map(|regex_match| regex_match.as_str())
     ).len() as i32
 }
 
-pub fn calculate_average_characters_of_words(text: &str, stop_list: Vec<String>) -> f32 {
-    let filter_set: HashSet<_> = stop_list.into_iter().collect();
-    let re = Regex::new(r"[a-zA-Z-]+").unwrap();
-
-    let matches: Vec<_> = re.find_iter(text)
-        .filter(
-            |regex_match| !filter_set.contains(regex_match.as_str())
-        ).collect();
+pub fn calculate_average_characters_of_words(text: &String, stop_list: &HashSet<String>) -> f32 {
+    let regex = Regex::new(WORD_REGEX).unwrap();
+    let matches: Vec<_> = find_matches(text, stop_list, &regex).collect();
     match matches.clone().into_iter().map(|regex_match| regex_match.len()).reduce(|acc, curr| acc + curr) {
         Some(value) => (value as f32) / (matches.len() as f32),
         None => 0f32
     }
+}
+
+fn find_matches<'a>(text: &'a String, stop_list: &'a HashSet<String>, regex: &'a Regex) -> impl Iterator<Item = Match<'a>> + 'a {
+    regex.find_iter(text)
+        .filter(
+            move |regex_match| !stop_list.contains(regex_match.as_str())
+        )
 }
 
 #[cfg(test)]
@@ -42,46 +39,46 @@ mod tests {
 
     #[test]
     fn test_count_words_little_lamb_poem() {
-        assert_eq!(5, count_words("marry had a little lamb", vec![]))
+        assert_eq!(5, count_words(&"marry had a little lamb".to_string(), &HashSet::new()))
     }
 
     #[test]
     fn test_count_words_text_with_two_whitespaces() {
-        assert_eq!(5, count_words("marry had a little  lamb", vec![]))
+        assert_eq!(5, count_words(&"marry had a little  lamb".to_string(), &HashSet::new()))
     }
 
     #[test]
     fn test_count_words_empty_string() {
-        assert_eq!(0, count_words("", vec![]))
+        assert_eq!(0, count_words(&"".to_string(), &HashSet::new()))
     }
 
     #[test]
     fn test_count_words_with_stopwords() {
-        assert_eq!(4, count_words("marry had a little lamb", vec!["the", "a", "on", "off"].iter().map(|s| { s.to_string()}).collect()))
+        assert_eq!(4, count_words(&"marry had a little lamb".to_string(), &["the", "a", "on", "off"].iter().map(|s| { s.to_string()}).collect()))
     }
 
     #[test]
     fn test_count_unique_words_with_duplicates() {
-        assert_eq!(11, count_unique_words("there are duplicates in this text, try to find them in the text!", vec![]))
+        assert_eq!(11, count_unique_words(&"there are duplicates in this text, try to find them in the text!".to_string(), &HashSet::new()))
     }
 
     #[test]
     fn test_count_unique_words_with_duplicates_and_stop_words() {
-        assert_eq!(10, count_unique_words("there are duplicates in this text, try to find them in the text!", vec!["the", "a", "on", "off"].iter().map(|s| { s.to_string()}).collect()))
+        assert_eq!(10, count_unique_words(&"there are duplicates in this text, try to find them in the text!".to_string(), &["the", "a", "on", "off"].iter().map(|s| { s.to_string()}).collect()))
     }
 
     #[test]
     fn test_count_words_with_hyphens() {
-        assert_eq!(10, count_words("Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.", vec![]))
+        assert_eq!(10, count_words(&"Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.".to_string(), &HashSet::new()))
     }
 
     #[test]
     fn test_count_unique_words_with_hyphens() {
-        assert_eq!(8, count_unique_words("Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.", vec![]))
+        assert_eq!(8, count_unique_words(&"Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.".to_string(), &HashSet::new()))
     }
 
     #[test]
     fn test_calculate_average_characters_of_words() {
-        assert_eq!(3.8, calculate_average_characters_of_words("marry had a little lamb", vec![]))
+        assert_eq!(3.8, calculate_average_characters_of_words(&"marry had a little lamb".to_string(), &HashSet::new()))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,40 @@
 use std::{env, fs};
+use std::collections::HashSet;
 use std::io::{stdin, stdout, Write};
 
 use wordcount::{calculate_average_characters_of_words, count_unique_words, count_words};
 
+
+const STOPWORDS_FILE_PATH: &str = "./stopwords.txt";
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let input_text = get_input_text(&args.get(1));
 
-    let s: String;
-    if args.len() > 1 {
-        let text_file_path = &args[1];
-        s = read_text_txt(text_file_path)
-    } else {
-        print!("Enter text: ");
-        s = read_user_input();
-    }
-
-    let stopwords: Vec<_> = read_stopwords_txt();
+    let stopwords: HashSet<_> = read_stopwords_txt();
 
     println!("Number of words: {}, unique: {}; average word length: {:.2} characters",
-             count_words(&s, stopwords.clone()),
-             count_unique_words(&s, stopwords.clone()),
-             calculate_average_characters_of_words(&s, stopwords.clone()))
+             count_words(&input_text, &stopwords),
+             count_unique_words(&input_text, &stopwords),
+             calculate_average_characters_of_words(&input_text, &stopwords))
 }
 
-fn read_text_txt(path: &str) -> String {
+fn get_input_text(text_file_path: &Option<&String>) -> String {
+    match text_file_path {
+        Some(path) => read_text_file(path),
+        None => {
+            print!("Enter text: ");
+            read_user_input()
+        }
+    }
+}
+
+fn read_text_file(path: &str) -> String {
     fs::read_to_string(path).unwrap_or_else(|_| String::new())
 }
 
-fn read_stopwords_txt() -> Vec<String> {
-    match fs::read_to_string("./stopwords.txt") {
-        Ok(val) => val.split("\n").into_iter().map(|s| s.to_string()).collect(),
-        Err(_) => vec![]
-    }
+fn read_stopwords_txt() -> HashSet<String> {
+    read_text_file(STOPWORDS_FILE_PATH).split("\n").into_iter().map(|s| s.to_string()).collect()
 }
 
 fn read_user_input() -> String {
@@ -44,7 +47,5 @@ fn read_user_input() -> String {
     if let Some('\r') = s.chars().next_back() {
         s.pop();
     }
-
-    {}
     s
 }
